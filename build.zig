090f998191d26f334bba7177b1c549ec8e1a1062
tiawl <pabtomas@protonmail.com>
2024-04-08 17:43:38 +0200

    [update] new wait paramater into run ()

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn copy (src: [] const u8, dest: [] const u8) !void
   try std.fs.copyFileAbsolute (src, dest, .{});
 }
 
-pub fn run (builder: *std.Build, proc: struct { argv: [] const [] const u8, cwd: ?[] const u8 = null, env: ?*const std.process.EnvMap = null, }) !void
+pub fn run (builder: *std.Build, proc: struct { argv: [] const [] const u8, cwd: ?[] const u8 = null, env: ?*const std.process.EnvMap = null, wait: ?*const fn () void = null, }) !void
 {
   var stdout = std.ArrayList (u8).init (builder.allocator);
   var stderr = std.ArrayList (u8).init (builder.allocator);
@@ -37,13 +37,19 @@ pub fn run (builder: *std.Build, proc: struct { argv: [] const [] const u8, cwd:
   child.env_map = proc.env;
 
   try child.spawn ();
-  try child.collectOutput (&stdout, &stderr, std.math.maxInt (usize));
 
-  const term = try child.wait ();
-
+  var term: std.ChildProcess.Term = undefined;
+  if (proc.wait) |wait|
+  {
+    wait ();
+    term = try child.kill ();
+  } else {
+    try child.collectOutput (&stdout, &stderr, std.math.maxInt (usize));
+    term = try child.wait ();
+  }
   if (stdout.items.len > 0) std.debug.print ("{s}", .{ stdout.items, });
   if (stderr.items.len > 0 and !std.meta.eql (term, std.ChildProcess.Term { .Exited = 0, })) std.debug.print ("\x1b[31m{s}\x1b[0m", .{ stderr.items, });
-  try std.testing.expectEqual (term, std.ChildProcess.Term { .Exited = 0, });
+  if (proc.wait == null) try std.testing.expectEqual (term, std.ChildProcess.Term { .Exited = 0, });
 }
 
 pub fn build (builder: *std.Build) !void
