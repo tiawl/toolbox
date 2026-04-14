d62011ca5eb4829e4415ece9a29789b0e2b73f49
tiawl <pabtomas@protonmail.com>
2026-04-14 23:10:34 +0200

    build.zig.zon: 0.16.0

--- build.zig
+++ build.zig
@@ -240,7 +240,7 @@ pub const VerboseBuilder = struct {
             } else {
                 latest = try self.run(&.{ "git", "rev-parse", "HEAD" }, tmp_dir);
             }
-            _ = try self.run(&.{ "zig", "fetch", "--save=" ++ field.name, self.fmt("git+https://{s}{s}.git#{s}", .{ host, path, latest }) }, self.ptrCwd().*);
+            _ = try self.run(&.{ "zig", "fetch", "--save=" ++ field.name, self.fmt("git+https://{s}{s}#{s}", .{ host, path, latest }) }, self.ptrCwd().*);
         }
     }
 
